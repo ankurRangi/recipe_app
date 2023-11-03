bdde8f50e7d93ec5ebae5d58e25dea1edc9e1205
Ankur <ankur.rangi08@gmail.com>
2023-11-04 03:49:37 +0530

    Base Admin Customization

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,48 @@
+"""
+Test for Django Admin Customization
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTest(TestCase):
+    """Test for Django Admin."""
+
+    def setUp(self):
+        """Create User and Client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="test123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@example.com",
+            password="testuser123",
+            name="Test User"
+        )
+    
+    def test_user_list(self):
+        """Test the user listed on the page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_edit_page(self):
+        """Test the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user(self):
+        """Test the user create page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
